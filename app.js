5b20c83c50dbe4bb5241f03eb3e44f5e75d75117
Bhargav Nookala <nooknb@gmail.com>
2014-05-23 09:31:00 -0700

    adding parse sdk

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
 	var map = L.mapbox.map('map', 'bnookala.ia5763g5').setView([37.7833, -122.4167], 12);
+	Parse.initialize("vOfd7H7ltgQUwEqNnZoasBSvXqOErfjUV4LOTIjZ", "vDLTR8myOnupvtQzyX0h3Q4zBwIYjMMiDabRwDbZ");
 
 	$.getJSON(
 		'parks_nice.geojson',
 		function (data) {
 			map.featureLayer.setGeoJSON(data);
 		}.bind(this)
-	)
+	);
 });
