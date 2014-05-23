8cd4b25a156ee58bccc2981cf4bc4285df84bc11
Bhargav Nookala <bhargav@140-sfoengwifi55-215.clients.corp.yelpcorp.com>
2014-05-22 18:51:19 -0700

    fixing parks_nice

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	var map = L.mapbox.map('map', 'bnookala.ia5763g5').setView([37.7833, -122.4167], 12);
 
 	$.getJSON(
-		'parks.geojson',
+		'parks_nice.geojson',
 		function (data) {
 			map.featureLayer.setGeoJSON(data);
 		}.bind(this)
